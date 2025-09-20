f977ea14b1c1eb4bf84def7941a2d5ebbd2ce39f
Kostov, Toni <toni.kostov@study.thws.de>
2025-09-20 16:37:43 +0300

    move weather details to own file in exercise 2

---
+++ mobile-applications-2-exercise-2-2/components/WeatherDetails.js
@@ -0,0 +1,45 @@
+import {StyleSheet, Text, View} from "react-native";
+import {MOCK_WEATHER} from "../utils/mockWeather";
+import {getWeatherEmoji} from "../utils/weatherCodeMap";
+
+export default function WeatherDetails() {
+    return (
+        <View style={styles.weatherContainer}>
+            <Text style={styles.cityText}>{MOCK_WEATHER.city}</Text>
+            <Text style={styles.icon}>{getWeatherEmoji(MOCK_WEATHER.code)}</Text>
+            <View style={styles.detailsContainer}>
+                <Text style={styles.detailsText}>{MOCK_WEATHER.temperatureC} °C</Text>
+                <Text style={styles.detailsText}>{MOCK_WEATHER.windKmh} km/h</Text>
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    weatherContainer: {
+        backgroundColor: 'rgba(255,255,255,0.8)',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 40,
+        borderRadius: 20
+    },
+    cityText: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    icon: {
+        fontSize: 150,
+        textAlign: 'center',
+    },
+    detailsContainer: {
+        flexDirection: 'row',
+        gap: 30
+    },
+    detailsText: {
+        fontSize: 25,
+        textAlign: 'center'
+    }
+});
--- mobile-applications-2-exercise-2-2/components/WeatherPanel.js
+++ mobile-applications-2-exercise-2-2/components/WeatherPanel.js
@@ -3,6 +3,7 @@ import {MOCK_WEATHER} from "../utils/mockWeather";
 import {getWeatherEmoji} from "../utils/weatherCodeMap";
 import PrimaryButton from "./PrimaryButton";
 import {useState} from "react";
+import WeatherDetails from "./WeatherDetails";
 
 export default function WeatherPanel() {
     const [loading, setLoading] = useState(false);
@@ -21,16 +22,7 @@ export default function WeatherPanel() {
             {
                 loading ? <ActivityIndicator size="large" color='white'/> :
                     <>
-                        <View style={styles.weatherContainer}>
-                            <Text style={styles.cityText}>{MOCK_WEATHER.city}</Text>
-                            <Text style={styles.icon}>{getWeatherEmoji(MOCK_WEATHER.code)}</Text>
-                            <View style={styles.detailsContainer}>
-                                <Text style={styles.detailsText}>{MOCK_WEATHER.temperatureC} °C</Text>
-                                <Text style={styles.detailsText}>{MOCK_WEATHER.windKmh} km/h</Text>
-
-                            </View>
-                        </View>
-
+                        <WeatherDetails/>
                         <PrimaryButton
                             text='Simulate loading'
                             onPress={handleLoading}/>
@@ -50,30 +42,5 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         gap: 20,
-    },
-    weatherContainer: {
-        backgroundColor: 'rgba(255,255,255,0.8)',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 40,
-        borderRadius: 20
-    },
-    cityText: {
-        fontSize: 40,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    icon: {
-        fontSize: 150,
-        textAlign: 'center',
-    },
-    detailsContainer: {
-        flexDirection: 'row',
-        gap: 30
-    },
-    detailsText: {
-        fontSize: 25,
-        textAlign: 'center'
     }
 });
