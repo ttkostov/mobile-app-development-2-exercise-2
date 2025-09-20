ba3f313ab67c5d6ad56c05c9a3aa984907c922b1
Kostov, Toni <toni.kostov@study.thws.de>
2025-09-20 16:55:02 +0300

    update some flex parameters on weather screen in exercise 3

--- mobile-applications-2-exercise-2-3/screens/WeatherScreen.js
+++ mobile-applications-2-exercise-2-3/screens/WeatherScreen.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         alignItems: 'stretch',
         justifyContent: 'flex-start',
-
+        gap: 20
     },
     weatherContainer: {
         flex: 3,
