9958eaa152af55f3c813b9449ddfaf0ee9a61f0a
Kostov, Toni <toni.kostov@study.thws.de>
2025-09-20 16:50:25 +0300

    move weather details into own file in exercise 3

---
+++ mobile-applications-2-exercise-2-3/components/WeatherDetails.js
@@ -0,0 +1,53 @@
+import {StyleSheet, Text, View} from "react-native";
+import {MOCK_WEATHER} from "../utils/mockWeather";
+import {getWeatherEmoji} from "../utils/weatherCodeMap";
+import {Card} from "react-native-paper";
+
+export default function WeatherDetails() {
+    return (
+        <Card style={styles.weatherContainer}>
+            <Card.Title title={MOCK_WEATHER.city} titleStyle={styles.cityText}/>
+            <Card.Content>
+                <Text style={styles.icon}>{getWeatherEmoji(MOCK_WEATHER.code)}</Text>
+                <View style={styles.detailsContainer}>
+                    <Text style={styles.temperatureText}>{MOCK_WEATHER.temperatureC} °C</Text>
+                    <Text style={styles.detailsText}>{MOCK_WEATHER.windKmh} km/h</Text>
+
+                </View>
+            </Card.Content>
+
+        </Card>
+    )
+}
+
+const styles = StyleSheet.create({
+    weatherContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 40,
+        borderRadius: 20,
+    },
+    cityText: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    icon: {
+        fontSize: 150,
+        textAlign: 'center',
+    },
+    detailsContainer: {
+        flexDirection: 'column',
+        gap: 10
+    },
+    detailsText: {
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    temperatureText: {
+        fontSize: 35,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    }
+});
--- mobile-applications-2-exercise-2-3/components/WeatherPanel.js
+++ mobile-applications-2-exercise-2-3/components/WeatherPanel.js
@@ -4,6 +4,7 @@ import {getWeatherEmoji} from "../utils/weatherCodeMap";
 import PrimaryButton from "./PrimaryButton";
 import {useState} from "react";
 import {Card} from "react-native-paper"
+import WeatherDetails from "./WeatherDetails";
 
 export default function WeatherPanel() {
     const [loading, setLoading] = useState(false);
@@ -18,33 +19,18 @@ export default function WeatherPanel() {
 
     return (
         <View style={styles.panelContainer}>
-
             {
                 loading ? <ActivityIndicator size="large" color='white'/> :
                     <>
-                        <Card style={styles.weatherContainer}>
-                            <Card.Title title={MOCK_WEATHER.city} titleStyle={styles.cityText}/>
-                            <Card.Content>
-                                <Text style={styles.icon}>{getWeatherEmoji(MOCK_WEATHER.code)}</Text>
-                                <View style={styles.detailsContainer}>
-                                    <Text style={styles.temperatureText}>{MOCK_WEATHER.temperatureC} °C</Text>
-                                    <Text style={styles.detailsText}>{MOCK_WEATHER.windKmh} km/h</Text>
-
-                                </View>
-                            </Card.Content>
-
-                        </Card>
-
+                        <WeatherDetails/>
                         <PrimaryButton
                             text='Simulate loading'
                             onPress={handleLoading}/>
                     </>
 
             }
-
         </View>
-    )
-        ;
+    );
 }
 
 const styles = StyleSheet.create({
@@ -55,34 +41,5 @@ const styles = StyleSheet.create({
         justifyContent: 'space-around',
         gap: 20,
     },
-    weatherContainer: {
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 40,
-        borderRadius: 20,
 
-    },
-    cityText: {
-        fontSize: 35,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    icon: {
-        fontSize: 150,
-        textAlign: 'center',
-    },
-    detailsContainer: {
-        flexDirection: 'column',
-        gap: 10
-    },
-    detailsText: {
-        fontSize: 20,
-        textAlign: 'center'
-    },
-    temperatureText: {
-        fontSize: 35,
-        textAlign: 'center',
-        fontWeight: 'bold'
-    }
 });
