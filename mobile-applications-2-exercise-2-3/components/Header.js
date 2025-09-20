89cb09cdd4bc34db36a33562ddd865c264ce1ae3
Kostov, Toni <toni.kostov@study.thws.de>
2025-09-20 18:35:37 +0300

    remove unused import

--- mobile-applications-2-exercise-2-3/components/Header.js
+++ mobile-applications-2-exercise-2-3/components/Header.js
@@ -1,5 +1,5 @@
 import {StyleSheet} from 'react-native';
-import {Appbar, useTheme} from 'react-native-paper';
+import {Appbar} from 'react-native-paper';
 
 
 export default function Header({toggleTheme}) {
@@ -23,5 +23,4 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 30
     }
-
 });
