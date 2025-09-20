006fd3d77cfc16701c5bee7c1c36326d85a9b198
Kostov, Toni <toni.kostov@study.thws.de>
2025-09-20 18:35:02 +0300

    add dark theme

--- mobile-applications-2-exercise-2-3/theme/AppTheme.js
+++ mobile-applications-2-exercise-2-3/theme/AppTheme.js
@@ -1,7 +1,7 @@
-import {MD3LightTheme} from 'react-native-paper';
+import {MD3DarkTheme, MD3LightTheme} from 'react-native-paper';
 import {Platform} from "react-native";
 
-export const AppTheme = {
+export const LightTheme = {
     ...MD3LightTheme,
     myOwnProperty: true,
     colors: {
@@ -19,6 +19,26 @@ export const AppTheme = {
             web:  'chocolate',
             default:  'darkslateblue',
         }),
-    }
-    ,
+    },
+};
+
+export const DarkTheme = {
+    ...MD3LightTheme,
+    myOwnProperty: true,
+    colors: {
+        ...MD3DarkTheme.colors,
+        primary:
+            Platform.select({
+                ios: 'darkslategray',
+                android: 'darkseagreen',
+                web:  'chocolate',
+                default:  'darkslateblue',
+            }),
+        background: Platform.select({
+            ios: 'black',
+            android: 'darkslategray',
+            web: 'darkred',
+            default: 'darkblue',
+        }),
+    },
 };
