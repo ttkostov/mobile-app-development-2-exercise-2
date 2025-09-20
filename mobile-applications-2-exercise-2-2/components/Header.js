a58411f33892d61a45924127a46f2563ea97d181
Kostov, Toni <toni.kostov@study.thws.de>
2025-09-20 16:44:13 +0300

    fix formatting

--- mobile-applications-2-exercise-2-2/components/Header.js
+++ mobile-applications-2-exercise-2-2/components/Header.js
@@ -1,26 +1,25 @@
-import { Text, View, StyleSheet } from 'react-native';
-
+import {Text, View, StyleSheet} from 'react-native';
 
 export default function Header() {
-  return (
-    <View style={styles.heatherContainer}>
-      <Text style={styles.text}>Weather Now</Text>
-    </View>
-  )
+    return (
+        <View style={styles.heatherContainer}>
+            <Text style={styles.text}>Weather Now</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  heatherContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(255,255,255,0.8)',
-    gap: 20,
-    borderRadius: 20
-  },
-  text: {
-    fontSize: 30
-  }
+    heatherContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(255,255,255,0.8)',
+        gap: 20,
+        borderRadius: 20
+    },
+    text: {
+        fontSize: 30
+    }
 
 });
